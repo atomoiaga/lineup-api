fe7e0f61ca672c2bd31f2411e2a69ff606f03ea5
astomoiaga <andrei.tomoiaga@bitstone.eu>
2024-07-10 18:48:45 +0300

    Add env var configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArtistModule } from './modules/artist/artist.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ArtistModule,
-    MongooseModule.forRoot('mongodb://localhost:27000/line-up'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('DATABASE_URL'),
+      }),
+    }),
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
